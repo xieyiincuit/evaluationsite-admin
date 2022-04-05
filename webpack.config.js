914c12559c59b3c0bbe7181673d37c3a637bef0e
zhousl <57696015+xieyiincuit@users.noreply.github.com>
2022-04-05 10:13:37 +0800

    #update webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     resolve: {
-        alias: {
-            path: require.resolve("path-browserify")
+        fallback: {
+            "path": require.resolve("path-browserify")
         }
-    }
+    },
 };
