a93b773657024982fe2b00729ea8efbf37b8023f
zhousl <57696015+xieyiincuit@users.noreply.github.com>
2022-04-20 22:39:43 +0800

    [#update] 首页统计功能

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,20 @@
   <div class="home-wrapper">
     <div class="date-row">
       <div v-for="(item, index) in cardInfoData" :key="index" class="data-col">
-        <el-card shadow="always" :body-style="{padding: '0px'}">
+        <el-card shadow="always" :body-style="{ padding: '0px' }">
           <div class="date-block">
-            <i :class="[ item.icon ]" :style="{ background: item.color }" class="icon-box" />
+            <i
+              :class="[item.icon]"
+              :style="{ background: item.color }"
+              class="icon-box"
+            />
             <div class="date-cont">
-              <CountTo class="count" :start-val="0" :end-val="item.count" :duration="3000" />
+              <CountTo
+                class="count"
+                :start-val="0"
+                :end-val="item.count"
+                :duration="3000"
+              />
               <p class="title">{{ item.title }}</p>
             </div>
           </div>
@@ -15,36 +24,62 @@
     </div>
     <el-row class="date-box" :gutter="20">
       <el-col :span="8">
-        <el-card shadow="always" :body-style="{padding: '0px'}">
-          <ChartsPie :title="text" type="pie" :data="chartsPieData" class="data-desc" />
+        <el-card shadow="always" :body-style="{ padding: '0px' }">
+          <ChartsPie
+            title="测评文章统计信息"
+            type="pie"
+            :data="articlePieData"
+            class="data-desc"
+          />
         </el-card>
       </el-col>
       <el-col :span="8">
-        <el-card shadow="always" :body-style="{padding: '0px'}">
+        <el-card shadow="always" :body-style="{ padding: '0px' }">
           <div class="data-desc data-lang-box">
-            <div class="data-lang-title">项目语言构成</div>
-            <div v-for="(item, index) in langsData" :key="index" class="data-lang-item">
+            <div class="data-lang-title">系统语言构成</div>
+            <div
+              v-for="(item, index) in langsData"
+              :key="index"
+              class="data-lang-item"
+            >
               <label>{{ item.name }}</label>
-              <el-progress :percentage="item.value" :stroke-width="16" :color="item.color" />
+              <el-progress
+                :percentage="item.value"
+                :stroke-width="16"
+                :color="item.color"
+              />
             </div>
           </div>
         </el-card>
       </el-col>
       <el-col :span="8">
-        <el-card shadow="always" :body-style="{padding: '0px'}">
-          <ChartsPie :title="text" type="ring" :data="chartsPieData" class="data-desc" />
+        <el-card shadow="always" :body-style="{ padding: '0px' }">
+          <ChartsPie
+            title="游戏录入统计信息"
+            type="ring"
+            :data="gameinfoPieData"
+            class="data-desc"
+          />
         </el-card>
       </el-col>
     </el-row>
     <el-row class="date-box" :gutter="20">
       <el-col :span="12">
-        <el-card shadow="always" :body-style="{padding: '0px'}">
-          <ChartsBar :title="title" :data="commonChartsData" class="data-chart" />
+        <el-card shadow="always" :body-style="{ padding: '0px' }">
+          <ChartsBar
+            :title="title"
+            :data="commonChartsData"
+            class="data-chart"
+          />
         </el-card>
       </el-col>
       <el-col :span="12">
-        <el-card shadow="always" :body-style="{padding: '0px'}">
-          <ChartsLine :title="title" :data="commonChartsData" class="data-chart" />
+        <el-card shadow="always" :body-style="{ padding: '0px' }">
+          <ChartsLine
+            :title="title"
+            :data="commonChartsData"
+            class="data-chart"
+          />
         </el-card>
       </el-col>
     </el-row>
@@ -61,38 +96,112 @@ export default {
   components: { CountTo, ChartsPie, ChartsBar, ChartsLine },
   data() {
     return {
-      text: '用户访问来源',
       title: '近七天用户访问量条形图',
       langsData: [
-        { name: 'VUE', value: 45, color: '#2d8cf0' },
-        { name: 'ES6', value: 32, color: '#19be6b' },
-        { name: 'TS', value: 4, color: '#ff9900' },
-        { name: 'LESS', value: 13, color: '#e46cbb' },
-        { name: 'HTML', value: 6, color: '#9a66e4' }
+        { name: 'C#', value: 52, color: '#2d8cf0' },
+        { name: 'Vue', value: 23, color: '#19be6b' },
+        { name: 'JavaScript', value: 10, color: '#ff9900' },
+        { name: 'CSS', value: 7, color: '#e46cbb' },
+        { name: 'HTML', value: 8, color: '#9a66e4' }
       ],
       cardInfoData: [
-        { title: '今日点击', icon: 'vue-dsn-icon-dianji', count: 682, color: '#2d8cf0' },
-        { title: '新增用户', icon: 'vue-dsn-icon-xinzeng', count: 259, color: '#19be6b' },
-        { title: '信息发送', icon: 'vue-dsn-icon-xinfeng', count: 1262, color: '#ff9900' },
-        { title: '点赞统计', icon: 'vue-dsn-icon-dianzan', count: 508, color: '#e46cbb' },
-        { title: '累计收藏', icon: 'vue-dsn-icon-heart', count: 379, color: '#9a66e4' }
+        {
+          title: '文章总数',
+          icon: 'vue-dsn-icon-dianji',
+          count: 0,
+          color: '#2d8cf0'
+        },
+        {
+          title: '用户总数',
+          icon: 'vue-dsn-icon-xinzeng',
+          count: 0,
+          color: '#19be6b'
+        },
+        {
+          title: '评论总数',
+          icon: 'vue-dsn-icon-xinfeng',
+          count: 0,
+          color: '#ff9900'
+        },
+        {
+          title: '游戏总数',
+          icon: 'vue-dsn-icon-dianzan',
+          count: 0,
+          color: '#e46cbb'
+        },
+        {
+          title: '商品总数',
+          icon: 'vue-dsn-icon-heart',
+          count: 0,
+          color: '#9a66e4'
+        }
       ],
-      chartsPieData: [
-        { value: 1920, name: '直接访问' },
-        { value: 1169, name: '邮件营销' },
-        { value: 986, name: '联盟广告' },
-        { value: 621, name: '视频广告' },
-        { value: 3256, name: '搜索引擎' }
+      articlePieData: [
+        { value: 2, name: '单机测评' },
+        { value: 3, name: '网游测评' },
+        { value: 1, name: '独立游戏' },
+        { value: 2, name: '手游测评' },
+        { value: 1, name: '电玩测评' }
+      ],
+      gameinfoPieData: [
+        { value: 2, name: '动作游戏' },
+        { value: 2, name: '网络游戏' },
+        { value: 1, name: '冒险游戏' },
+        { value: 3, name: '角色扮演' },
+        { value: 1, name: '竞速游戏' }
       ],
       commonChartsData: {
-        Mon: 782,
-        Tue: 925,
-        Wed: 1196,
-        Thu: 812,
-        Fri: 328,
-        Sat: 222,
-        Sun: 1080
+        Mon: 25,
+        Tue: 45,
+        Wed: 23,
+        Thu: 75,
+        Fri: 87,
+        Sat: 113,
+        Sun: 198
       }
+    }
+  },
+  mounted() {
+    this.showArticle()
+    this.showGame()
+    this.showUser()
+  },
+  methods: {
+    showArticle() {
+      this.$http.get('v1/b/statistics/articles', null, (res) => {
+        this.cardInfoData[0].count = res.articleCount
+        this.cardInfoData[2].count = res.commentCount
+        let index = 0
+        for (var key in res.categoryMap) {
+          this.articlePieData[index].name = key
+          this.articlePieData[index].value = res.categoryMap[key]
+
+          if (index === 5) {
+            break
+          }
+          index++
+        }
+      })
+    },
+    showGame() {
+      this.$http.get('v1/b/statistics/games', null, (res) => {
+        this.cardInfoData[3].count = res.gameCount
+        this.cardInfoData[4].count = res.shopCount
+        let index = 0
+        for (var key in res.categoryMap) {
+          this.articlePieData[index].name = key
+          this.articlePieData[index].value = res.categoryMap[key]
+          if (index === 5) {
+            break
+          }
+          index++
+        }
+      })
+    },
+    showUser() {
+      this.$http.get('v1/b/statistics/users', null, (res) => {
+        this.cardInfoData[1].count = res.userCount
+      })
     }
   }
 }
